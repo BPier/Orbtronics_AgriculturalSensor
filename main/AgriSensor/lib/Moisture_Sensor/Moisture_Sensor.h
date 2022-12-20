0087f7445f5db7a7e442a14f9f906d8113a01548
BPier <bpierrick@gmail.com>
2022-12-20 14:25:08 -0400

    Adding library for ph and Moisture debug

---
+++ main/AgriSensor/lib/Moisture_Sensor/Moisture_Sensor.h
@@ -0,0 +1,17 @@
+#ifndef Moisture_Sensor_h
+#define Moisture_Sensor_h
+
+#include "Arduino.h"
+
+class MoistureSensor
+{
+  public:
+    MoistureSensor(int pin);
+    void setup();
+    float read();
+    void calibrate();
+  private:
+    int _pin;
+};
+
+#endif
--- main/AgriSensor/src/main.cpp
+++ main/AgriSensor/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <pH_Sensor.h>
+#include <Moisture_Sensor.h>
+
+// pH Variables
+pHSensor pH_S(33);
+float pH_Value = 0.0;
+
+// Moisture Variable
+// Moisture is displayed has Volumetric Water Content from 0-100%
+MoistureSensor Moist_S(35);
+float Moisture_Value = 0.0;
 
 void setup() {
-  
+  Serial.begin(115200);
+  Serial.println("======= SETUP =======");
+
+  pH_S.setup();
+  Moist_S.setup();
+
+  Serial.println("=====================");
 }
 
 void loop() {
+  Serial.println("---------------------");
+
+  // Read and display the pH Value
+  pH_Value = pH_S.read();
+  Serial.print("[DATA] - Soil pH is ");
+  Serial.println(pH_Value);
+
+  // Read and Display the Volumetric Water Content
+  Moisture_Value = Moist_S.read();
+  Serial.print("[DATA] - Soil VWC (Volumetric Water Content) is ");
+  Serial.print(Moisture_Value,0);
+  Serial.println("%");
+
+
+  delay(5000);
 
 }
