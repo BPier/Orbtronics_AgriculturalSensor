7a192a5067689953516a73c63259695467bf3a1b
BPier <bpierrick@gmail.com>
2022-12-21 14:42:02 -0400

    Add LIbraries for Main

---
+++ main/AgriSensor/lib/Data_Storage/Data_Storage.cpp
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+#include "Data_Storage.h"
+
+
+DataStorage::DataStorage()
+{
+  // pinMode(pin, INPUT);
+  // _pin = pin;
+  return;
+}
+
+void DataStorage::setup()
+{
+    Serial.println("[INFO] : The data module is ready to be used ");
+}
+
+
+void DataStorage::read()
+{
+
+}
+
+void DataStorage::writedata()
+{
+
+}
