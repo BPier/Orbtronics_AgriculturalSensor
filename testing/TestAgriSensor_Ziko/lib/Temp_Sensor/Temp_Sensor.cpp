fb7e7a87a97044fd00b18a0856a8106dbd51094c
DustmanZ <zikodiego@hotmail.com>
2022-12-23 13:39:42 -0400

    Update Temp_Sensor.cpp

--- testing/TestAgriSensor_Ziko/lib/Temp_Sensor/Temp_Sensor.cpp
+++ testing/TestAgriSensor_Ziko/lib/Temp_Sensor/Temp_Sensor.cpp
@@ -3,12 +3,8 @@
 #include <stdio.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-int Temp_Pin = 17;
 
-// Setup a oneWire instance to communicate with any OneWire device
-OneWire oneWire(Temp_Pin);	
-// Pass oneWire reference to DallasTemperature library
-DallasTemperature sensors(&oneWire);
+int Temp_Pin = 0;
 
 TempSensor::TempSensor(int pin)
 {
@@ -19,21 +15,17 @@ TempSensor::TempSensor(int pin)
 
 void TempSensor::setup()
 {
-  sensors.begin();    
-  Serial.print("[INFO] : The Temperature sensor is set up on pin ");
-  Serial.println(Temp_Pin);
+    Serial.print("[INFO] : The Temperature sensor is set up on pin ");
+    Serial.println(Temp_Pin);
 }
 
 
 float TempSensor::read()
 {
-  // Generate a random Value between 10 and 35C to simulate soil Temperature
-  // float TempValue = random(100,350);
-  // TempValue = TempValue/10;
-  // Send the command to get temperatures
-  sensors.requestTemperatures(); 
-  return sensors.getTempCByIndex(0);
-  // return TempValue;
+    // Generate a random Value between 10 and 35C to simulate soil Temperature
+    float TempValue = random(100,350);
+    TempValue = TempValue/10;
+    return TempValue;
 }
 
 void TempSensor::calibrate()
