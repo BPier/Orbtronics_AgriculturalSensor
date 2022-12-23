5a68ad98c24aea38b436a619796473627bcae609
BPier <bpierrick@gmail.com>
2022-12-23 16:30:34 -0400

    update DataStorage

--- main/AgriSensor/lib/Data_Storage/Data_Storage.cpp
+++ main/AgriSensor/lib/Data_Storage/Data_Storage.cpp
@@ -27,11 +27,22 @@ void DataStorage::writedata(float pH, float Moisture, float Temp)
   
   static char time[30];
   Timelib Time_l;
-  char* Formated_time = "EMPTY";
+  char* Formated_time = "";
+  static char Formated_text[255];
   unsigned long TimeMillis = 0;
   TimeMillis = Time_l.GetTime();
   Formated_time = Time_l.FormatTime(TimeMillis);
 
-  Serial.println("[INFO] : The following data is stored ");
-  Serial.println(Formated_time);
+  snprintf(Formated_text,
+    255,
+    PSTR("{\"Time\":\"%s\",\"pH\":%.2f,\"Moisture\":%.1f,\"Temp\":%.2f}"),
+    Formated_time,
+    pH,
+    Moisture,
+    Temp
+  );
+
+  Serial.print("[INFO] : The following data is stored: ");
+  Serial.println(Formated_text);
+
 }
